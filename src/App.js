4b73564899d52908add431c9b94fb650e31ef69e
princess <princessmumbi001@gmail.com>
2024-04-29 14:36:09 +0300

    adds component file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect } from 'react';
+import Ttables from '../src/components/Ttables';
+import Tsearch from '../src/components/Tsearch';
+import Tforms from '../src/components/Tforms';
 
 function App() {
+  const initialTransactions = [
+    {date: '2024-12-02', description:"ordered pizza",category: "Food", amount:2250},
+    {date: '2024-11-06', description:"paid for movie tickets",category: "Entertainment", amount:850},
+    {date: '2024-06-14', description:"bought clothes",category: "Clothing", amount:8000},
+    {date: '2024-02-18', description:"fueled my bentley",category: "Transport", amount:6700},
+    {date: '2024-12-05', description:"bought make-up",category: "Beauty and skincare", amount:14000},
+    
+
+  ];
+  const [transactions,setTransactions] = useState(initialTransactions);
+  const [filteredTransactions,setFilteredTransactions] =useState(transactions);
+
+
+  function handleAddTransaction(formData){
+    setTransactions([...transactions, formData]);
+    setFilteredTransactions([...transactions, formData]);
+  };
+
+  function handleSearch(searchTerm){
+    const filteredTransactions = transactions.filter(transaction =>
+    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  setFilteredTransactions(filteredTransactions);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>BANK TRANSACTION</h1>
+      <Tforms onAddTransaction={handleAddTransaction} />
+      <Tsearch onSearch={handleSearch} />
+      <Ttables transactions={filteredTransactions} />
     </div>
+   
   );
 }
-
 export default App;
---
+++ src/components/Tsearch.jsx
@@ -0,0 +1,15 @@
+import React, {useState} from 'react';
+
+function Tsearch({onSearch}){
+    const[searchTerm, setSearchTerm]= useState('');
+
+    function handleChange(e){
+        setSearchTerm(e.target.value);
+        onSearch(e.target.value);
+        };
+
+        return(
+            <input id='search' type="text" value={searchTerm} onChange={handleChange} placeholder="Search transactions..."/>
+        );
+}
+export default Tsearch
